7d287308a66f9c034a1706f73c5c55c13287903c
Tim Wu <timwuchia@gmail.com>
2018-04-30 17:35:29 -0700

    about us
    about us done

--- archive-team-member.php
+++ archive-team-member.php
@@ -13,6 +13,36 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+			<h2 class="team-title">Our Team</h2>
+
+			<?php 
+
+				$args = array(
+					'post_type' => 'team-member',
+					'posts_per_page' => -1
+				);
+
+				$members = new WP_Query( $args );
+				if($members->have_posts()){
+
+					echo "<div class='all-team-members'>";
+					while($members->have_posts()){
+						$members->the_post();
+						echo "<div class='team-member'>";
+						the_post_thumbnail("medium");
+						echo "<h3>";
+						the_title();
+						echo "</h3>";
+						the_content();
+						echo "</div>";
+					}
+					echo "</div>";
+				}
+
+
+
+			 ?>
+
 		
 		</main><!-- #main -->
 	</div><!-- #primary -->

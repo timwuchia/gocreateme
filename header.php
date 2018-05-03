378876919b13d31691a74ad5b3f7abfc7c5186f2
Jeff Hung <egriftinc@gmail.com>
2018-05-03 18:26:58 -0400

    Added fonts
    added fonts

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css?family=Alex+Brush" rel="stylesheet">
+		<link rel="stylesheet" href="ostrich-regular.ttf" type="text/css" charset="utf-8" /> 
 	<script
 	  src="https://code.jquery.com/jquery-3.3.1.min.js"
 	  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -32,7 +33,7 @@
 	<header id="masthead" class="site-header">
 		<nav id="site-navigation" class="main-navigation">
 			<ul class='logo'>
-				<li><a href="<?php echo get_home_url(); ?>"><span class="go">Go</span> Create <span class="me">Me</span></a></li>
+				<li><a href="<?php echo get_home_url(); ?>"><span class="go">Go</span>Create<span class="me">Me</span></a></li>
 			</ul>
 			<?php
 			wp_nav_menu( array(
